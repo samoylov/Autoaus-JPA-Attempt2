aaed2e4f8bbdd1d024ed7697b0812abcae1d366e
kostya <kostya.samoylov@gmail.com>
2016-11-23 12:07:39 +0200

    update

--- src/main/java/autohaus/controller/ColorController.java
+++ src/main/java/autohaus/controller/ColorController.java
@@ -5,6 +5,7 @@ import autohaus.service.ColorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,20 +19,24 @@ public class ColorController {
     @RequestMapping(value = "/newColor", method = RequestMethod.GET)
     public String newColor(Model model) {
         model.addAttribute("colors", colorService.findAll());
+        model.addAttribute("addColor", new Color(""));
         return "newColor";
     }
 
     @RequestMapping(value = "/saveColor", method = RequestMethod.POST)
-    public String saveColor(@RequestParam String name) {
-        Color color = new Color(name);
-        colorService.save(color);
+    public String saveColor(/*@RequestParam String name*/
+                            @ModelAttribute Color addColor) {
+        colorService.save(addColor);
         return "redirect:/newColor";
     }
 
     @RequestMapping(value = "/deleteColor", method = RequestMethod.POST)
     public String deleteColor(@RequestParam String[] ids) {
-        for (String id : ids)
+        for (String id : ids) {
+            System.out.println(id);
             colorService.delete(Integer.parseInt(id));
+        }
         return "redirect:/newColor";
     }
+
 }
--- src/main/webapp/WEB-INF/views/newColor.jsp
+++ src/main/webapp/WEB-INF/views/newColor.jsp
@@ -1,26 +1,28 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="sf" %>
+
 <html>
 <head>
     <title>new Color</title>
 </head>
 <body>
 
-<%@include file="navigation.html" %>
+<%@ include file="navigation.html" %>
 
 <h1>Color</h1>
 
 <h2>New Color</h2>
 
-<form action="saveColor" method="post">
-    <input name="name" placeholder="color name" autofocus="autofocus">
+<sf:form modelAttribute="addColor" action="saveColor" method="post">
+    <sf:input path="name" placeholder="color name" autofocus="autofocus"/>
     <button>save</button>
-</form>
+</sf:form>
 
 <h2>Existing Colors</h2>
 
 <form action="deleteColor" method="post">
-    <select size="10" name="ids" style="width: 170px;" multiple="multiple">
+    <select multiple="multiple" size="10" name="ids">
         <c:forEach var="color" items="${colors}">
             <option value="${color.id}">
                     ${color.name}
